a14978edbbda22ac92dbc80261ff1ce614e72362
Yazan Al Monshed <yazanmonshed@outlook.com>
2018-10-18 20:55:56 +0300

    comress files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-var gulp = require('gulp'),
-    image= require('gulp-image'); 
+/* global, Global, function */
 
+// varibles here 
 
-
+var gulp   = require('gulp'),
+    image  = require('gulp-image'),       // Comporss images
+    uglify = require('gulp-uglify'),     // Compress JavaScript 
+    clean  = require('gulp-clean-css'), // Compress Css
+    rename = require('gulp-rename');    // Rename Files compress from style.css To style.min.css Like That 
+   
 // function Default
 
 gulp.task('gulp', function() {
@@ -10,8 +15,11 @@ gulp.task('gulp', function() {
     console.log('here gulp');
 });
 
+// End Function Default    
+ 
 //Function Min 
  
+// Function Minfy 
 gulp.task('min', function() {
     
     gulp.src('img/*')
@@ -20,8 +28,30 @@ gulp.task('min', function() {
     
 });
 
+//End Function Minify
 
 
-// call here 
+// End Function Min 
+gulp.task('compress', function() {
+    gulp.src('js/*')
+    .pipe(uglify())
+    .pipe(rename({suffix:'.min'})) // Rename Name Js 
+    .pipe(gulp.dest('js/minify'));
+});
 
-gulp.task('default',['min']);
+gulp.task('clean', function () {
+    
+    gulp.src('css/*.css')
+    .pipe(clean())
+    .pipe(rename({suffix:'.min'}))  // Rename Name Css 
+    .pipe(gulp.dest('css/css-min'));
+});
+
+
+
+// Call here 
+
+gulp.task('default',['min','compress','clean']);
+
+
+// End Call      
